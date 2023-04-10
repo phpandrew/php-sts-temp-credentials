236eeef34463fecc49ed96ab073e78bea7675617
phpandrew <lylesback2@hotmail.com>
2023-04-10 08:09:14 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
-<?php
-
 require_once("signaturev4.php");
 
 $awssig = new awssignature();
+
+$awssig->setTokens("Atza|your_token_here");
 
 $data = "";
 $method = "GET";
@@ -11,6 +11,6 @@ $queryUrl = "";
 $uri = "/sellers/v1/marketplaceParticipations";
 $requestUrl = "https://$host$uri?$queryUrl";
 
-$headers = $this->calculateSignature($host, $uri, $queryUrl, $requestUrl, $awssig->getAmzAccessKey(), $awssig->getAmzAccessSecret(), "us-east-1", "execute-api", $method, $data);
-$json = $this->sendRequest($requestUrl, $method,  $headers, $data);
-echo "json == $json";
+$headers = $awssig->calculateSignature($host, $uri, $queryUrl, $requestUrl, $awssig->getAmzAccessKey(), $awssig->getAmzAccessSecret(), "us-east-1", "execute-api", $method, $data);
+$json = $awssig->sendRequest($requestUrl, $method,  $headers, $data);
+print_r($json);
