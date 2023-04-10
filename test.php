33421d803141d334407544a9c43304cff6049ebf
phpandrew <lylesback2@hotmail.com>
2023-04-09 21:10:10 -0400

    Create test.php
    Created a test file for example use

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("signaturev4.php");
+
+$awssig = new awssignature();
+
+$data = "";
+$method = "GET";
+$host = $this->api_host;
+$queryUrl = "";
+$uri = "/sellers/v1/marketplaceParticipations";
+$requestUrl = "https://$host$uri?$queryUrl";
+
+$headers = $this->calculateSignature($host, $uri, $queryUrl, $requestUrl, $awssig->getAmzAccessKey(), $awssig->getAmzAccessSecret(), "us-east-1", "execute-api", $method, $data);
+$json = $this->sendRequest($requestUrl, $method,  $headers, $data);
+echo "json == $json";
+
+$reply = json_decode($json['response'], true);
