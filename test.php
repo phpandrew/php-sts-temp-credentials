44b4d53010f6f22181416376a85a1ea27a2c6413
phpandrew <lylesback2@hotmail.com>
2023-04-09 21:12:09 -0400

    Update test.php

--- test.php
+++ test.php
@@ -14,5 +14,3 @@ $requestUrl = "https://$host$uri?$queryUrl";
 $headers = $this->calculateSignature($host, $uri, $queryUrl, $requestUrl, $awssig->getAmzAccessKey(), $awssig->getAmzAccessSecret(), "us-east-1", "execute-api", $method, $data);
 $json = $this->sendRequest($requestUrl, $method,  $headers, $data);
 echo "json == $json";
-
-$reply = json_decode($json['response'], true);
